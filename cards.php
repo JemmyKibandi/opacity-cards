c186bd1cda0b1281594e3fb71515194894f268a9
Kibandi ✨ <101172276+JemmyKibandi@users.noreply.github.com>
2024-03-22 12:43:35 +0300

    Create cards.php

---
+++ cards.php
@@ -0,0 +1,106 @@
+<?php
+session_start();
+<!DOCTYPE html>
+<html lang="en" dir="ltr" data-nav-layout="vertical" data-theme-mode="light" data-header-styles="light"
+    data-menu-styles="light" data-toggled="close">
+
+<head>
+</head>
+
+<body>
+    <div class="page">
+ 
+        <!-- Start::app-content -->
+        <div class="main-content app-content">
+            <div class="container-fluid">
+ <div class="card-body">
+                                        <div class="table-responsive">
+                                            <div class="row">
+                                                <div class="col-xxl-3 col-lg-4 col-md-6">
+                                                    <a href="index.php?id=222">
+                                                        <div class="card custom-card border-top-card border-top-primary rounded-0"
+                                                            style="background-image: url('../User/pic2.png'); background-size: cover;">
+                                                            <div class="card-body"
+                                                                style="background-color: rgba(0, 128, 0, 0.6);">
+                                                                <div class="text-center">
+                                                                    <span
+                                                                        class="avatar avatar-md bg-primary shadow-sm avatar-rounded mb-2">
+                                                                        <i class="ri-phone-find-line fs-16"></i> </span>
+                                                                    <p class="fs-14 fw-semibold mb-2"
+                                                                        style="color: white;">
+                                                                        All Contacts</p>
+
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </a>
+                                                </div>
+                                                <div class="col-xxl-3 col-lg-4 col-md-6">
+                                                    <a href="index.php?id=666">
+                                                        <div class="card custom-card border-top-card border-top-primary rounded-0"
+                                                            style="background-image: url('../User/pic2.png'); background-size: cover;">
+                                                            <div class="card-body"
+                                                                style="background-color: rgba(0, 128, 0, 0.6);">
+                                                                <div class="text-center">
+                                                                    <span
+                                                                        class="avatar avatar-md bg-primary shadow-sm avatar-rounded mb-2">
+                                                                        <i class="ri-folder-open-line fs-16"></i>
+                                                                    </span>
+                                                                    <p class="fs-14 fw-semibold mb-2"
+                                                                        style="color: white;">
+                                                                        File Manager</p>
+
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </a>
+                                                </div>
+                                                <div class="col-xxl-3 col-lg-4 col-md-6">
+                                                    <a href="index.php?id=666">
+                                                        <div class="card custom-card border-top-card border-top-primary rounded-0"
+                                                            style="background-image: url('../User/pic2.png'); background-size: cover;">
+                                                            <div class="card-body"
+                                                                style="background-color: rgba(0, 128, 0, 0.6);">
+                                                                <div class="text-center">
+                                                                    <span
+                                                                        class="avatar avatar-md bg-primary shadow-sm avatar-rounded mb-2">
+                                                                        <i class="ri-wallet-line fs-16"></i>
+                                                                    </span>
+                                                                    <p class="fs-14 fw-semibold mb-2"
+                                                                        style="color: white;">
+                                                                        Sales</p>
+
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </a>
+                                                </div>
+                                                <div class="col-xxl-3 col-lg-4 col-md-6">
+                                                    <a href="index.php?id=666">
+                                                        <div class="card custom-card border-top-card border-top-primary rounded-0"
+                                                            style="background-image: url('../User/pic2.png'); background-size: cover;">
+                                                            <div class="card-body"
+                                                                style="background-color: rgba(0, 128, 0, 0.6);">
+                                                                <div class="text-center">
+                                                                    <span
+                                                                        class="avatar avatar-md bg-primary shadow-sm avatar-rounded mb-2">
+                                                                        <i class="ri-user-5-line fs-16"></i>
+                                                                    </span>
+                                                                    <p class="fs-14 fw-semibold mb-2"
+                                                                        style="color: white;">
+                                                                        Leads</p>
+
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+</div>
+</div>
+</div>
+</body>
+
+</html>
